2030941a3f24e599a66cede9d6861023caad3c74
miller9 <millerpoli9@gmail.com>
2020-06-18 11:50:24 -0500

    1-EdBase. Actualizo controlador articles para editar, vista y ruta.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
-  get "/home", to: "home#index"
+  get "home", to: "home#index"
 
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
   get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+  patch "articles/:id", to: "articles#update", as: :article
 
 end
