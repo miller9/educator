39572e3e5c6d448e8f9a16c826cdbde9dd906f6a
miller9 <millerpoli9@gmail.com>
2020-06-19 13:38:51 -0500

    1-EdBase. Establezco relacion *a* entre tablas articles y types.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   has_rich_text :content
   belongs_to :educator
   has_many :has_types
+  has_many :types, through: :has_types
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,4 @@
 class Type < ApplicationRecord
   has_many :has_types
+  has_many :articles, through: :has_types
 end
