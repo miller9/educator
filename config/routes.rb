72aa22ffa9dffe95e2766860f6304ad0739e1718
miller9 <millerpoli9@gmail.com>
2020-06-19 11:39:34 -0500

    1-EdBase. Actualizo rutas con 'resources' y deshabilito new article para educators no logueados.

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get "home", to: "home#index"
 
-  get "articles/educator/:educator_id", to: "articles#from_author"
 
-  get "articles", to: "articles#index"
-  get "articles/new", to: "articles#new", as: :new_articles
-  post "articles", to: "articles#create"
-  get "articles/:id", to: "articles#show"
-  get "articles/:id/edit", to: "articles#edit"
-  patch "articles/:id", to: "articles#update", as: :article
-  delete "articles/:id", to: "articles#destroy"
+  resources :articles do
+    get "educator/:educator_id", to: "articles#from_author", on: :collection
+  end
+
 
 end
