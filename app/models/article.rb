1a2ecc8fbb42c5a53cd38ac4ea8ed45907dbf018
miller9 <millerpoli9@gmail.com>
2020-06-19 15:14:26 -0500

    1-EdBase. Durante la edicion de un article, se borran los types previamente asociados a dicho article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,7 @@ class Article < ApplicationRecord
 
   def save_types
     return has_types.destroy_all if type_elements.nil? || type_elements.empty?
+    has_types.where.not(type_id: type_elements).destroy_all
     type_elements.each do |type_id|
       HasType.find_or_create_by(article: self, type_id: type_id)
     end
