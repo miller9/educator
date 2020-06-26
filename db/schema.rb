ce268bc9d2889b7168ddf7f0998c1dec373bf400
miller9 <millerpoli9@gmail.com>
2020-06-26 17:04:16 -0500

    3-EdCommentsV1. Migracion.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_025119) do
+ActiveRecord::Schema.define(version: 2020_06_26_215838) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2020_06_20_025119) do
     t.index ["educator_id"], name: "index_articles_on_educator_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "educators", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -88,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_06_20_025119) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "educators"
+  add_foreign_key "comments", "articles"
   add_foreign_key "has_types", "articles"
   add_foreign_key "has_types", "types"
 end
