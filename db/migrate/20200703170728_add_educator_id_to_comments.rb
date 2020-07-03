9e985f1f22f6faadbc6a68e68af1ec745b13b607
miller9 <millerpoli9@gmail.com>
2020-07-03 12:10:11 -0500

    4-EdCommAssociation. Hago rollback para corregir migracion eliminando comments por default para educator '1'.

--- db/migrate/20200703170728_add_educator_id_to_comments.rb
+++ db/migrate/20200703170728_add_educator_id_to_comments.rb
@@ -1,5 +1,5 @@
 class AddEducatorIdToComments < ActiveRecord::Migration[6.0]
   def change
-    add_reference :comments, :educator, null: false, foreign_key: true, default: 1
+    add_reference :comments, :educator, null: false, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_001127) do
+ActiveRecord::Schema.define(version: 2020_07_03_170728) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_07_03_001127) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "educator_id", default: 1, null: false
+    t.integer "educator_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["educator_id"], name: "index_comments_on_educator_id"
   end
